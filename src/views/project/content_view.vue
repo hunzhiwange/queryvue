02978ae86a13028e1c3a01b3bad3c08a44456e0e
XiangMin Liu <635750556@qq.com>
2022-07-02 13:45:43 +0800

    111

--- src/views/project/content_view.vue
+++ src/views/project/content_view.vue
@@ -1,72 +1,56 @@
 <template>
-    <div class="layout">
-        <Layout>
-            <div class="header">
-                <Menu mode="horizontal" theme="light" active-name="1">
-                    <Row>
-                        <Col span="6">
-                            <MenuItem name="2" :to="'/board/' + projectIssue.project.num">
-                                <Icon type="md-list" />
-                                {{ projectIssue.project.name }}
-                            </MenuItem>
-                        </Col>
-                        <Col span="12">
-                            <div style="text-align: center">
-                                <MenuItem name="1" :to="'/board/issue/' + projectIssue.num">
-                                    <Icon type="md-arrow-round-back" />
-                                    {{ projectIssue.title }}
-                                </MenuItem>
-                            </div>
-                        </Col>
-                        <Col span="6">
-                            <div class="pull-right">
-                                <MenuItem name="3" :to="'/project/content/' + projectIssue.num">
-                                    <Icon type="md-create" />
-                                    编辑
-                                </MenuItem>
-                                <MenuItem name="4">
-                                    <Icon type="md-share" />
-                                    分享
-                                </MenuItem>
-                            </div>
-                        </Col>
-                    </Row>
-                </Menu>
-            </div>
-
-            <div class="doc-content-view-max">
-                <mavonEditor
-                    v-if="projectIssue.project_type.content_type != 6"
-                    v-model="projectIssue.project_content.content"
-                    :subfield="editProp.subfield"
-                    :defaultOpen="editProp.defaultOpen"
-                    :toolbarsFlag="editProp.toolbarsFlag"
-                    :editable="editProp.editable"
-                    :scrollStyle="editProp.scrollStyle"
-                    :boxShadow="editProp.boxShadow"
-                    previewBackground="#FFFFFF"
-                />
-            </div>
-        </Layout>
+    <div class="body">
+        <div class="wrap layout2">
+            <Row :gutter="16" class="m-t-10">
+                <Col span="24">
+                    <div class="header">
+                        <Menu mode="horizontal" theme="light" active-name="1">
+                            <Row type="flex" justify="center" align="middle">
+                                <Col span="6">
+                                    <MenuItem name="2" :to="'/board/' + projectIssue.project.num">
+                                        <Icon type="md-list" />
+                                        {{ projectIssue.project.name }}
+                                    </MenuItem>
+                                </Col>
+                                <Col span="12">
+                                    <MenuItem name="1" :to="'/board/issue/' + projectIssue.num">
+                                        <Icon type="md-arrow-round-back" />
+                                        {{ projectIssue.title }}
+                                    </MenuItem>
+                                </Col>
+                                <Col span="6">
+                                    <div class="pull-right">
+                                        <MenuItem name="3" :to="'/project/content/' + projectIssue.num">
+                                            <Icon type="md-create" />
+                                            编辑
+                                        </MenuItem>
+                                        <MenuItem name="4">
+                                            <Icon type="md-share" />
+                                            分享
+                                        </MenuItem>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Menu>
+                    </div>
+                    <div class="doc-content">
+                        <mavonEditor
+                            v-if="projectIssue.project_type.content_type != 6"
+                            v-model="projectIssue.project_content.content"
+                            :subfield="editProp.subfield"
+                            :defaultOpen="editProp.defaultOpen"
+                            :toolbarsFlag="editProp.toolbarsFlag"
+                            :editable="editProp.editable"
+                            :scrollStyle="editProp.scrollStyle"
+                            :boxShadow="editProp.boxShadow"
+                            previewBackground="#FFFFFF"
+                        />
+                    </div>
+                </Col>
+            </Row>
+        </div>
     </div>
 </template>
-
-<style scoped>
-.layout {
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    height: 100%;
-}
-.layout .ivu-menu-horizontal {
-    /* height: 33px;
-    line-height: 33px; */
-}
-.layout-footer-center {
-    text-align: center;
-}
-</style>
 
 <script src="./assets/content_view.js"></script>
 <style lang="less" src="./assets/content_view.less"></style>
